c42be3d71d9a16cec8e6d20a1eee273bf10d587e
boukrisabiha <sabiha97boukri@gmail.com>
2025-01-26 16:30:52 +0100

    commit -  product liste - item style

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
