41eb6b2f8e4bd2514d2779e2ec96a9218ba22cd7
widad123 <khatiri.widad@gmail.com>
2025-02-02 00:09:41 +0100

    add header with cart icon to show a cart content and to access to the cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -4,11 +4,12 @@ import {CartService} from '../../services/cart.service';
 import {CommonModule} from '@angular/common';
 import {MatIcon} from '@angular/material/icon';
 import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
+import {MatButton, MatIconButton, MatMiniFabButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, MatIcon, MatCardContent, MatCardTitle, MatCard],
+  imports: [CommonModule, MatIcon, MatCardContent, MatCardTitle, MatCard, MatMiniFabButton, MatIconButton, MatButton],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatMenu, MatMenuTrigger} from '@angular/material/menu';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {CartComponent} from '../cart/cart.component';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatIcon,
+    MatMenu,
+    CartComponent,
+    MatMenuTrigger,
+    MatIconButton,
+    MatButton
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  constructor(private router: Router) {}
+
+  goToCart() {
+    this.router.navigate(['/cart']);
+  }
 }
