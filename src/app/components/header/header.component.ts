4d21f2af85aa8f539a3e31fd2e9ac7fa40d310ff
boukrisabiha <sabiha97boukri@gmail.com>
2025-02-02 23:13:16 +0100

    SBO- router link , redirection vers products

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,7 +4,7 @@ import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {CartComponent} from '../cart/cart.component';
 import {MatButton, MatIconButton} from '@angular/material/button';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -15,7 +15,8 @@ import {Router} from '@angular/router';
     CartComponent,
     MatMenuTrigger,
     MatIconButton,
-    MatButton
+    MatButton,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
