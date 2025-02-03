d153f7ac4b144f06c471c38fe853677014f18fcf
boukrisabiha <sabiha97boukri@gmail.com>
2025-02-03 01:05:30 +0100

    SBO- Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,16 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import {HomeComponent} from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isHeaderVisible = true;
+  lastScrollTop = 0;
 
+  ngOnInit(): void {
+    this.observeScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > this.lastScrollTop) {
+      this.isHeaderVisible = false;
+    } else {
+      this.isHeaderVisible = true;
+    }
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+
+    this.observeScroll();
+  }
+
+  observeScroll(): void {
+    const contentSection = document.querySelector('.content');
+    if (contentSection) {
+      const rect = contentSection.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 100) {
+        contentSection.classList.add('visible');
+      }
+    }
+  }
 }
