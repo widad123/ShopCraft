06e189f4c3bb4410c46e7ab855813559b2b25fbc
AsmaeTnani <asmaetnani@gmail.com>
2025-02-25 10:24:02 +0100

    Validation of the commande

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import {HomeComponent} from './components/home/home.component';
+import { OrderFormComponent } from './components/order-form/order-form.component';
+import {PaymentSuccessComponent} from './components/payment-success/payment-success.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -17,5 +19,7 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'orderForm' , component: OrderFormComponent},
+  { path: 'payment-success' , component: PaymentSuccessComponent},
   { path: '**', component: NotFoundComponent },
 ];
